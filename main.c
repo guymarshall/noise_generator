7eea0f43be71ac2c92e4485171bcfed93c2c31aa
Guy Marshall <guymarshall9@gmail.com>
2025-12-19 14:23:01 +0000

    UPDATE: Now takes arguments from user input

--- main.c
+++ main.c
@@ -2,26 +2,35 @@
 #include <stdlib.h>
 #include <time.h>
 
-// TODO: take user input for number of frames, frames per second, width, height
 // TODO: call ffmpeg to create video e.g. "ffmpeg -i output-%02d.ppm -r 60 output.mp4"
 // TODO: clean up all .ppm files
 // TODO: make README.md
 
 #define MAX 255
 
-int main(void)
+int main(int argc, char **argv)
 {
+	if (argc != 5)
+	{
+		fprintf(stderr, "Incorrect argument count of %d.\n", argc);
+		fprintf(stderr, "USAGE: ./main {frame_count} {frames_per_second} {width} {height}\n");
+
+		return 1;
+	}
+
+	int frame_count = atoi(argv[1]);
+	// int frames_per_second = atoi(argv[2]);
+	int width = atoi(argv[3]);
+	int height = atoi(argv[4]);
+
 	srand(time(NULL));
 
 	char buffer[256];
-	for (int i = 0; i < 60; i++)
+	for (int i = 0; i < frame_count; i++)
 	{
 		snprintf(buffer, sizeof(buffer), "output-%02d.ppm", i);
 		const char *output_path = buffer;
 		FILE *file = fopen(output_path, "wb");
-
-		int width = 16 * 60;
-		int height = 9 * 60;
 
 		fprintf(file, "P6\n");
 		fprintf(file, "%d %d\n", width, height);
