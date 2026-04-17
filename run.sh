a6545ace853647f4b84b967b5cdb6fb6cbbae12d
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2026-04-17 21:41:19 +0100

    Initial commit

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+gcc -Wall -Wextra -Werror -O3 -o main main.c
+
+mkdir -p target
+
+mv main target/main
+
+frame_count=60
+frames_per_second=60
+width=1280
+height=720
+./target/main "$frame_count" "$frames_per_second" "$width" "$height"
